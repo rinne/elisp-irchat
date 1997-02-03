6519307f59de60260e110e75ad03922626a55416
tri <tri>
1997-02-03 21:24:26 +0000

    Fixed documentation of the variable irchat-client-x-face.

--- irchat-vars.el
+++ irchat-vars.el
@@ -1,6 +1,6 @@
 ;;;  -*- emacs-lisp -*-
 ;;;
-;;;  $Id: irchat-vars.el,v 1.3 1997/01/31 13:01:48 too Exp $
+;;;  $Id: irchat-vars.el,v 1.4 1997/02/03 21:24:26 tri Exp $
 ;;;
 ;;; see file irchat-copyright.el for change log and copyright info
 
@@ -205,7 +205,7 @@ Default is what (system-name) returns.")
   "*Userinfo message given to anyone asking.")
 
 (defvar irchat-client-x-face ""
-  "*X-Face message given to anyone asking.")
+  "*X-Face message given to anyone asking.  Proper form is \"X-Face: ....\".")
 
 (defvar irchat-keepalive-interval nil
   "*Interval on seconds the existence of server connection is checked")
