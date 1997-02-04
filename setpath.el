fc4c0f614e37d41c57e1f37667fc1e84f7fa8855
tri <tri>
1997-02-04 00:03:31 +0000

    Do not load defsubst.el if major version of emacs is >= 19.

--- setpath.el
+++ setpath.el
@@ -1,8 +1,11 @@
 ;;;  -*- emacs-lisp -*-
 ;;;
-;;;  $Id: setpath.el,v 1.1 1996/12/19 14:54:53 tri Exp $
+;;;  $Id: setpath.el,v 1.2 1997/02/04 00:03:31 tri Exp $
 ;;;
 ;;;  set path for compiling irchat
 
 (setq load-path (append (list ".") load-path))
-(load "defsubst")
+
+;;; Emacs 19 has a builtin defsubst.
+(if (> 19 (string-to-int (substring emacs-version 0 2)))
+    (load "defsubst"))
