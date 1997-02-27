e8da258dd6cc89da8ed377bf047f4516ee9a9f19
tri <tri>
1997-02-27 13:29:27 +0000

    Made Japanese Rot-47 optional.  No-one uses it anyway with irchat. Left the code in though.

--- irchat-caesar.el
+++ irchat-caesar.el
@@ -1,37 +1,53 @@
 ;;;  -*- emacs-lisp -*-
 ;;;
-;;;  $Id: irchat-caesar.el,v 3.1 1997/02/24 16:00:02 tri Exp $
+;;;  $Id: irchat-caesar.el,v 3.2 1997/02/27 13:29:27 tri Exp $
 ;;;
 ;;; caesar-region written by phr@prep.ai.mit.edu  Nov 86
 ;;; Modified by tower@prep Nov 86
 ;;; Modified by umerin@flab.flab.Fujitsu.JUNET for ROT47.
-;;; Modified by tri@iki.fi for irchat
+;;; Modified by tri@iki.fi for irchat (japanese is now optional)
 
 (eval-and-compile (provide 'irchat-caesar))
 
-(defun irchat-caesar-region (&optional n)
+(defun irchat-caesar-region (&optional n &optional japanese)
   "Caesar rotation of region by N, default 13, for decrypting netnews.
 ROT47 will be performed for Japanese text in any case."
   (interactive (if current-prefix-arg	; Was there a prefix arg?
 		   (list (prefix-numeric-value current-prefix-arg))
-		 (list nil)))
+		 (list nil nil)))
   (cond ((not (numberp n)) (setq n 13))
 	((< n 0) (setq n (- 26 (% (- n) 26))))
 	(t (setq n (% n 26))))		;canonicalize N
-  (if (not (zerop n))		; no action needed for a rot of 0
-      (progn
-	(if (or (not (boundp 'caesar-translate-table))
-		(/= (aref caesar-translate-table ?a) (+ ?a n)))
+  (if (not (zerop n))			; no action needed for a rot of 0
+      (progn 
+	(if (and (not japanese)
+		 (or (not (boundp 'caesar-translate-table-1))
+		     (/= (aref caesar-translate-table-1 ?a) (+ ?a n))))
 	    (let ((i 0) (lower "abcdefghijklmnopqrstuvwxyz") upper)
-	      (message "Building caesar-translate-table...")
-	      (setq caesar-translate-table (make-vector 256 0))
+	      (message "Building caesar-translate-table-1...")
+	      (setq caesar-translate-table-1 (make-vector 256 0))
 	      (while (< i 256)
-		(aset caesar-translate-table i i)
+		(aset caesar-translate-table-1 i i)
 		(setq i (1+ i)))
 	      (setq lower (concat lower lower) upper (upcase lower) i 0)
 	      (while (< i 26)
-		(aset caesar-translate-table (+ ?a i) (aref lower (+ i n)))
-		(aset caesar-translate-table (+ ?A i) (aref upper (+ i n)))
+		(aset caesar-translate-table-1 (+ ?a i) (aref lower (+ i n)))
+		(aset caesar-translate-table-1 (+ ?A i) (aref upper (+ i n)))
+		(setq i (1+ i)))
+	      (message "Building caesar-translate-table 1... done")))
+	(if (and japanese
+		 (or (not (boundp 'caesar-translate-table-2))
+		     (/= (aref caesar-translate-table-2 ?a) (+ ?a n))))
+	    (let ((i 0) (lower "abcdefghijklmnopqrstuvwxyz") upper)
+	      (message "Building caesar-translate-table-2...")
+	      (setq caesar-translate-table-2 (make-vector 256 0))
+	      (while (< i 256)
+		(aset caesar-translate-table-2 i i)
+		(setq i (1+ i)))
+	      (setq lower (concat lower lower) upper (upcase lower) i 0)
+	      (while (< i 26)
+		(aset caesar-translate-table-2 (+ ?a i) (aref lower (+ i n)))
+		(aset caesar-translate-table-2 (+ ?A i) (aref upper (+ i n)))
 		(setq i (1+ i)))
 	      ;; ROT47 for Japanese text.
 	      ;; Thanks to ichikawa@flab.fujitsu.junet.
@@ -40,13 +56,16 @@ ROT47 will be performed for Japanese text in any case."
 		    (t2 (logior ?! 128))
 		    (t3 (logior ?~ 128)))
 		(while (< i 256)
-		  (aset caesar-translate-table i
-			(let ((v (aref caesar-translate-table i)))
+		  (aset caesar-translate-table-2 i
+			(let ((v (aref caesar-translate-table-2 i)))
 			  (if (<= v t1) (if (< v t2) v (+ v 47))
 			    (if (<= v t3) (- v 47) v))))
 		  (setq i (1+ i))))
-	      (message "Building caesar-translate-table... done")))
-	(let ((from (region-beginning))
+	      (message "Building caesar-translate-table 2... done")))
+	(let ((caesar-translate-table (if japanese 
+					  caesar-translate-table-2
+					caesar-translate-table-1))
+	      (from (region-beginning))
 	      (to (region-end))
 	      (i 0) str len)
 	  (setq str (buffer-substring from to))
@@ -57,6 +76,7 @@ ROT47 will be performed for Japanese text in any case."
 	  (goto-char from)
 	  (delete-region from to)
 	  (insert str)))))
+
 ;;;
 ;;; eof
 ;;;
