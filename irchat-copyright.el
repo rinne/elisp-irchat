c9cd5e928d2771dc756d7351c68c00dfc95ab76a
tri <tri>
1997-07-09 13:46:04 +0000

    Made some small modifications.  Copyright notice of course remains intact.

--- irchat-copyright.el
+++ irchat-copyright.el
@@ -1,11 +1,14 @@
 ;;;  -*- emacs-lisp -*-
 ;;;
-;;;  $Id: irchat-copyright.el,v 3.2 1997/02/27 10:19:14 jsl Exp $
+;;;  $Id: irchat-copyright.el,v 3.3 1997/07/09 13:46:04 tri Exp $
 ;;;
 (defvar irchat-copyright "
-    $Id: irchat-copyright.el,v 3.2 1997/02/27 10:19:14 jsl Exp $
+    $Id: irchat-copyright.el,v 3.3 1997/07/09 13:46:04 tri Exp $
     Internet Relay CHAT interface for GNU Emacs
-    Copyright (C) 1989 Tor Lillqvist
+
+    Copyright © 1989 Tor Lillqvist <tml@iki.fi>
+
+    Copyright © 1990-1997 Irchat Developer Team    (see below)
  
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -21,22 +24,20 @@
     along with GNU Emacs; if not, write to the Free Software
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
   
-    Author's email address is <tml@hemuli.atk.vtt.fi>
-  
-    Following people have made major improvements to irchat. Also there
-    are numerous people who have given suggestions and improvements. If 
-    you think your name should be included here, give us a note...
+    Following people have made major improvements to irchat. Also 
+    there are numerous people who have given suggestions and
+    improvements. If you think your name should be included here,
+    give us a note...
+	   Markku Järvinen	<mta@iki.fi>
 	   Kai Keinänen		<kmk@iki.fi>
-	   Markku Järvinen	<mta@cc.tut.fi>
-	   Jukka Partanen      	<jtp@iki.fi>
-	   Kim Nyberg          	<kny@iki.fi>
+	   Janne Liimatainen	<jsl@iki.fi>
 	   Tero T. Mononen     	<tmo@iki.fi>
+	   Kim Nyberg          	<kny@iki.fi>
+           Tomi Ollila          <too@iki.fi>
+	   Jukka Partanen      	<jtp@iki.fi>
+	   Timo J. Rinne	<tri@iki.fi>
 
     You can ftp the latest version from cs.hut.fi:/pub/irchat
-
-    There is a mailing list, irchat@cc.tut.fi for those interested
-    in developments in irchat. Mail irchat-request@cc.tut.fi if you
-    are interested in joining.
 
     Current team is:
 	   Janne Liimatainen	<jsl@iki.fi>
@@ -45,16 +46,22 @@
            Tomi Ollila          <too@iki.fi>
 	   Jukka Partanen      	<jtp@iki.fi>
 	   Timo J. Rinne	<tri@iki.fi>
+
+    Bug reports and suggestions can be mailed to irchat-developers@cs.hut.fi.
 " "Basically GPL")
 
 ;;;
 ;;;  $Log: irchat-copyright.el,v $
+;;;  Revision 3.3  1997/07/09 13:46:04  tri
+;;;  Made some small modifications.  Copyright notice of course
+;;;  remains intact.
+;;;
 ;;;  Revision 3.2  1997/02/27 10:19:14  jsl
 ;;;  changed irchat-nickname to irchat-real-nickname
 ;;;  users should only change irchat-nickname
 ;;;
-;;;Revision 3.1  1997/02/24  16:00:02  tri
-;;;Promoted revision to 3.1.
+;;;  Revision 3.1  1997/02/24  16:00:02  tri
+;;;  Promoted revision to 3.1.
 ;;;
 ;;;  Revision 1.2  1997/02/13 09:10:47  tri
 ;;;  Added Domo to "current" team.
