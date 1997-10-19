3aa2adeb91d2b313ca847929c41fa7eb41341df1
tri <tri>
1997-10-19 19:28:39 +0000

    Initial revision

---
+++ irchat-obsolete.el
@@ -0,0 +1,79 @@
+;;;  -*- emacs-lisp -*-
+;;;
+;;;  $Id: irchat-obsolete.el,v 1.1 1997/10/19 19:28:39 tri Exp $
+;;;
+;;; see file irchat-copyright.el for change log and copyright info
+
+(eval-when-compile (require 'irchat-inlines))
+
+(defun irchat-Command-kill (kill-nickname-var &optional timeout silent)
+  (interactive (let ((kill-nickname-var nil)
+		     (timeout nil)
+		     (completion-ignore-case t))
+		 (setq kill-nickname-var 
+		       (completing-read "Ignore nickname or regexp: " 
+					(append irchat-nick-alist
+						irchat-kill-nickname)
+					'(lambda (s) t) nil nil))
+		 (if (and (not (string= "" kill-nickname-var))
+			  (not (assoc-ci-string kill-nickname-var irchat-kill-nickname)))
+		     (setq timeout
+			   (string-to-int
+			    (read-from-minibuffer "Timeout [RET for none]: "))))
+		 (list kill-nickname-var timeout)))
+
+  (message (concat "Obsolete interface irchat-Command-kill."
+		   "  "
+		   "Use irchat-Command-ignore."))
+  (irchat-Command-ignore kill-nickname-var timeout silent))
+  
+
+(defun irchat-Command-kill-by-regexp (kill-regexp-var &optional timeout silent)
+  (interactive (let ((kill-regexp-var nil)
+		     (timeout nil)
+		     (completion-ignore-case t))
+		 (setq kill-regexp-var 
+		       (completing-read "Ignore messages matching: " 
+					irchat-kill-message-regexp
+					'(lambda (s) t) nil nil))
+		 (if (and (not (string= "" kill-regexp-var))
+			  (not (assoc-ci-string kill-regexp-var
+						irchat-kill-message-regexp)))
+		     (setq timeout
+			   (string-to-int
+			    (read-from-minibuffer
+			     "Timeout [RET for none]: "))))
+		 (list kill-regexp-var timeout)))
+
+  (message (concat "Obsolete interface irchat-Command-kill-by-regexp."
+		   "  "
+		   "Use irchat-Command-ignore-by-regexp."))
+  (irchat-Command-ignore-by-regexp kill-regexp-var timeout silent))
+
+(defun irchat-Command-global-kill (nickname timeout reason)
+  (interactive (let ((nickname-var nil)
+		     (timeout 0)
+		     (reason ""))
+		 (setq nickname
+		       (completing-read "Global ignore nickname: "
+					irchat-nick-alist
+					'(lambda (s) t) nil nil))
+		 (if (not (string= "" nickname))
+		     (progn
+		       (while (not (and (> timeout 0)
+					(< timeout 1000)))
+			 (setq timeout
+			       (string-to-int
+				(read-from-minibuffer "Timeout [1-99]: "))))
+		       (setq reason (read-from-minibuffer "Reason: "))))
+		 (list nickname timeout reason)))
+
+  (message (concat "Obsolete interface irchat-Command-global-kill."
+		   "  "
+		   "Use irchat-Command-global-ignore."))
+  (irchat-Command-global-ignore nickname timeout reason))
+
+(eval-and-compile (provide 'irchat-obsolete))
+;;;
+;;; eof
+;;;
