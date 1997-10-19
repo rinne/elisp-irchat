5ee9af9e0affe9c7c2da4537daee3e0023f38b27
tri <tri>
1997-10-19 15:27:02 +0000

    Initial revision

---
+++ irchat-global-kill.el
@@ -0,0 +1,210 @@
+;;;  -*- emacs-lisp -*-
+;;;
+;;;  $Id: irchat-global-kill.el,v 1.1 1997/10/19 15:27:02 tri Exp $
+;;;
+;;; see file irchat-copyright.el for change log and copyright info
+;;;
+;;; Handle global kill ctcp messages by jsl and tri.
+;;;
+;;; Created              : Mon Mar 10 17:07:39 1997 tri
+;;; Integrated to Irchat : Sun Oct 19          1997 tri
+;;;
+
+(eval-when-compile (require 'irchat-inlines))
+
+(defvar irchat-global-kill-valid-senders '()
+  "*List of strings or string-int pairs containing valid IGNORE senders.")
+
+(defvar irchat-global-kill-use-uah-p nil
+  "*Use user-at-host in ignore command if possible")
+
+(defvar irchat-global-kill-use-silent-ignore nil
+  "*If non-nil, no expire messages are shown.")
+
+(defvar irchat-global-kill-default-timeout-limit 99
+  "*Default timeout limit for valid senders.")
+
+(defvar irchat-global-kill-report-ignored nil
+  "*How to report ignored kill request. (nil, t, 'minibuffer or 'dialogue)")
+
+;(setq irchat-global-kill-valid-senders 
+;      (list ("mypal@.*\\.hut.fi" . 600) ;; max 600 minutes from my pal
+;            (".*@.*\\.hut\\.fi" . 30)   ;; max 30 minutes from .hut.fi
+;             \".*@.*\\.fi\")            ;; max default (99) minutes from .fi
+
+(defun irchat-remove-from-ignore-list (name)
+  "Remove NAME from ignore list, if there."
+  (let ((elem (assoc-ci-string name irchat-kill-nickname)))
+    (if elem
+	(setq irchat-kill-nickname
+	      (remassoc (car elem) irchat-kill-nickname))))
+  irchat-kill-nickname)
+
+
+(defun irchat-global-kill-valid-sender (uah)
+  (let ((l irchat-global-kill-valid-senders)
+	(r nil))
+    (while l
+      (let* ((ca (car l))
+	     (ua (if (consp ca) (car ca) ca))
+	     (lm (if (consp ca) 
+		     (cdr ca) 
+		   irchat-global-kill-default-timeout-limit)))
+	(if (and (string-match ua uah)
+		 (> lm 0))
+	    (progn
+	      (setq l '())
+	      (setq r lm))
+	  (setq l (cdr l)))))
+    r))
+
+
+(defun irchat-ctl-a-ignore-msg-hook (from rest)
+  (if (and (stringp rest)
+	   (string-match "^\\(.+\\) \\([0-9][0-9]*\\) \\(.+\\)" rest))
+      (let ((nick (matching-substring rest 1))
+	    (timeout (string-to-int (matching-substring rest 2)))
+	    (reason (matching-substring rest 3))
+	    (valid-sender (irchat-global-kill-valid-sender irchat-userathost)))
+	(if (string= nick (regexp-quote nick))
+	    (if valid-sender
+		(let* ((nick-uah (if irchat-global-kill-use-uah-p
+				     (irchat-nick-to-uah nick)
+				   nil))
+		       (ign-name (if nick-uah
+				     nick-uah
+				   nick))
+		       (ign-list (if nick-uah
+				     (irchat-convert-uah-to-ignore-list 
+				      nick-uah)
+				   '()))
+		       (real-timeout (if (> timeout valid-sender)
+					 valid-sender
+				       timeout))
+		       (bad-timeout-p (not (= timeout real-timeout))))
+		  (irchat-w-insert irchat-D-buffer
+				   (format 
+			        "*** %s: Ignoring %s%s for %d minutes%s (%s)\n"
+				    from
+				    ign-name
+				    (if (string= nick ign-name)
+					""
+				      (concat " (" nick ")"))
+				    real-timeout
+				    (if bad-timeout-p
+					(format " [%d minutes requested]"
+						timeout)
+				      "")
+				    reason))
+		  (if (not ign-list)
+		      (if (not (assoc-ci-string ign-name 
+						irchat-kill-nickname))
+			  (irchat-Command-kill 
+			   (downcase ign-name)
+			   real-timeout 
+			   irchat-global-kill-use-silent-ignore))
+		    (let ((l ign-list))
+		      (while l
+			(if (not (assoc-ci-string (car l)
+						  irchat-kill-nickname))
+			    (irchat-Command-kill 
+			     (downcase (car l))
+			     real-timeout
+			     irchat-global-kill-use-silent-ignore))
+			(setq l (cdr l))))))
+	      (let ((msg (format "Rejected IGNORE from %s <%s> (%s, %d, %s)."
+				 from irchat-userathost nick timeout reason)))
+		(if (or (equal 'minibuffer
+			       irchat-global-kill-report-ignored)
+			(equal t
+			       irchat-global-kill-report-ignored))
+		    (message msg))
+		(if (or (equal 'dialogue
+			       irchat-global-kill-report-ignored)
+			(equal t
+			       irchat-global-kill-report-ignored))
+		    (irchat-w-insert irchat-D-buffer (concat irchat-info-prefix
+							     msg
+							     "\n"))))))))
+  t)
+
+
+(defun irchat-Command-global-kill (nickname timeout reason)
+  (interactive (let ((nickname-var nil)
+		     (timeout 0)
+		     (reason ""))
+		 (setq nickname
+		       (completing-read "Global ignore nickname: "
+					irchat-nick-alist
+					'(lambda (s) t) nil nil))
+		 (if (not (string= "" nickname))
+		     (progn
+		       (while (not (and (> timeout 0)
+					(< timeout 1000)))
+			 (setq timeout
+			       (string-to-int
+				(read-from-minibuffer "Timeout [1-99]: "))))
+		       (setq reason (read-from-minibuffer "Reason: "))))
+		 (list nickname timeout reason)))
+  (if (not (string= "" nickname))
+      (let* ((k1 (format "%s-ignore" irchat-current-channel))
+	     (k2 (format "%s-kill" irchat-current-channel))
+	     (k3 irchat-current-channel)
+	     (key (if (assoc-ci-string k1 irchat-default-idea-key-list)
+		      k1
+		    (if (assoc-ci-string k2 irchat-default-idea-key-list)
+			k2
+		      (if (assoc-ci-string k3 irchat-default-idea-key-list)
+			k3
+			nil)))))
+	(if (null key)
+	    (irchat-w-insert irchat-D-buffer
+			     (format 
+			      "*** error: no default key (%s, %s or %s) set\n"
+			      k1 k2 k3))
+	  (let ((msg (irchat-encrypt-message
+		      (format "IGNORE %s %d %s" nickname timeout reason)
+		      key t)))
+	    ; Ignore
+	    (let* ((nick-uah (if irchat-global-kill-use-uah-p
+				 (irchat-nick-to-uah nickname)
+			       nil))
+		   (ign-name (if nick-uah
+				 nick-uah
+			       nickname))
+		   (ign-list (if nick-uah
+				 (irchat-convert-uah-to-ignore-list 
+				  nick-uah)
+			       '())))
+	      (irchat-w-insert irchat-D-buffer
+			       (format 
+			     "*** Globally ignoring %s%s for %d minutes (%s)\n"
+				ign-name
+				(if (string= nickname ign-name)
+				    ""
+				  (concat " (" nickname ")"))
+				timeout
+				reason))
+	      (if (not ign-list)
+		  (progn
+		    (irchat-remove-from-ignore-list ign-name)
+		    (irchat-Command-kill (downcase ign-name)
+					 timeout
+					 irchat-global-kill-use-silent-ignore))
+		(let ((l ign-list))
+		  (while l
+		    (irchat-remove-from-ignore-list (car l))
+		    (irchat-Command-kill (downcase (car l))
+					 timeout 
+					 irchat-global-kill-use-silent-ignore)
+		    (setq l (cdr l)))))
+	      ; Send ctcp
+	      (irchat-send "PRIVMSG %s :%s" irchat-current-channel msg)))))))
+
+
+(setq irchat-ctl-a-ignore-msg-hook (function irchat-ctl-a-ignore-msg-hook))
+
+(eval-and-compile (provide 'irchat-global-kill))
+;;;
+;;; eof
+;;;
---
+++ irchat-uah-cache.el
@@ -0,0 +1,83 @@
+;;;  -*- emacs-lisp -*-
+;;;
+;;;  $Id: irchat-uah-cache.el,v 1.1 1997/10/19 15:27:06 tri Exp $
+;;;
+;;; see file irchat-copyright.el for change log and copyright info
+;;;
+;;; Nick to user-at-host cache by jsl.
+;;; Modifications by tri.
+;;;
+;;; Created              : Mon Mar 10 17:07:39 1997 tri
+;;; Integrated to Irchat : Sun Oct 19          1997 tri
+;;;
+
+(eval-when-compile (require 'irchat-inlines))
+
+(defconst irchat-nick-to-uah-vector-length 128
+  "*Vector length")
+
+(defvar irchat-nick-to-uah-pos 0
+  "Current update position")
+
+(defvar irchat-nick-to-uah-vector nil
+  "*Vector to store nick to user-at-host mappings")
+
+
+(defun irchat-nick-to-uah-init ()
+  (setq irchat-nick-to-uah-vector
+	(make-vector irchat-nick-to-uah-vector-length (cons "" "")))
+  (setq irchat-nick-to-uah-pos 0))
+
+
+(defun irchat-nick-to-uah-append (nick uah)
+  "Append NICK UAH tuple into the vector"
+  (if (null irchat-nick-to-uah-vector)
+      (irchat-nick-to-uah-init))
+  (if (and (stringp nick) (stringp uah))
+      (progn
+	(aset irchat-nick-to-uah-vector irchat-nick-to-uah-pos (cons nick uah))
+	(setq irchat-nick-to-uah-pos (mod (+ irchat-nick-to-uah-pos 1)
+					  irchat-nick-to-uah-vector-length)))))
+
+
+(defun irchat-nick-to-uah (nick)
+  "Find uah associated with NICK from cache if possible."
+  (if (null irchat-nick-to-uah-vector)
+      (irchat-nick-to-uah-init))
+  (let ((pos (if (= irchat-nick-to-uah-pos 0)
+		 (- irchat-nick-to-uah-vector-length 1)
+	       (- irchat-nick-to-uah-pos 1))))
+    (while (and (not (string-ci-equal nick (car (elt irchat-nick-to-uah-vector
+						     pos))))
+		(not (= pos irchat-nick-to-uah-pos)))
+      (setq pos (if (= pos 0) 
+		    (- irchat-nick-to-uah-vector-length 1)
+		  (- pos 1))))
+    (if (string-ci-equal nick (car (elt irchat-nick-to-uah-vector pos)))
+	(cdr (elt irchat-nick-to-uah-vector pos))
+      nil)))
+
+
+(defun irchat-convert-uah-to-ignore-list (uah)
+  "Convert UAH-string to list of regexps to be ignored."
+  (let ((usr nil)
+	(dom nil))
+    (cond ((string-match "^\\([^ \t@][^ \t@]*\\)@[^ \t@][^ \t@]*\\.\\([^ \t@.][^ \t@.]*\\.[^ \t@.][^ \t@.]*\\)$"
+			 uah)
+	   (setq usr (substring uah (match-beginning 1) (match-end 1)))
+	   (setq dom (substring uah (match-beginning 2) (match-end 2))))
+	  ((string-match "^\\([^ \t@][^ \t@]*\\)@\\([^ \t@.][^ \t@.]*\\.[^ \t@.][^ \t@.]*\\)$"
+			 uah)
+	   (setq usr (substring uah (match-beginning 1) (match-end 1)))
+	   (setq dom (substring uah (match-beginning 2) (match-end 2)))))
+    (if (and usr dom)
+	(progn
+	  (setq usr (regexp-quote usr))
+	  (setq dom (regexp-quote dom))
+	  (list (format "%s@%s" usr dom) (format "%s@.*\\.%s" usr dom)))
+      (list (regexp-quote uah)))))
+
+(eval-and-compile (provide 'irchat-uah-cache))
+;;;
+;;; eof
+;;;
