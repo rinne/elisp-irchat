f871a7cec9a968869f6cd1a4ef88619b1b6ee783
tri <tri>
1997-12-11 06:05:32 +0000

    Irchat mkdist.

--- irchat-version.el
+++ irchat-version.el
@@ -1,6 +1,6 @@
 ;;;  -*- emacs-lisp -*-
 ;;;
-;;;  $Id: irchat-version.el,v 3.64 1997/12/01 08:20:32 tri Exp $
+;;;  $Id: irchat-version.el,v 3.65 1997/12/11 06:05:32 tri Exp $
 ;;;
 ;;; see file irchat-copyright.el for change log and copyright info
 
@@ -20,7 +20,7 @@
 (defconst irchat-client-version-alpha-p t
   "Is this version an alpha version?")
 
-(defconst irchat-client-version-release "$Date: 1997/12/01 08:20:32 $"
+(defconst irchat-client-version-release "$Date: 1997/12/11 06:05:32 $"
   "version release date")
 
 (defconst irchat-client-name "Irchat"
