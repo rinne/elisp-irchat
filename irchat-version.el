d29b6981511b38625ebee2548e5068656f3f8d78
tri <tri>
1997-12-12 06:54:14 +0000

    Made it 3.04alpha.

--- irchat-version.el
+++ irchat-version.el
@@ -1,6 +1,6 @@
 ;;;  -*- emacs-lisp -*-
 ;;;
-;;;  $Id: irchat-version.el,v 3.67 1997/12/12 06:52:17 tri Exp $
+;;;  $Id: irchat-version.el,v 3.68 1997/12/12 06:54:14 tri Exp $
 ;;;
 ;;; see file irchat-copyright.el for change log and copyright info
 
@@ -11,16 +11,16 @@
 (defconst irchat-client-version-major "3"
   "Major version number. Major functional changes change this number.")
 
-(defconst irchat-client-version-minor "03"
+(defconst irchat-client-version-minor "04"
   "Minor version number. Server Protocol changes and bug fixes change this number.")
 
-(defconst irchat-client-version-beta-p nil
+(defconst irchat-client-version-beta-p t
   "Is this version a beta version?")
 
-(defconst irchat-client-version-alpha-p nil
+(defconst irchat-client-version-alpha-p t
   "Is this version an alpha version?")
 
-(defconst irchat-client-version-release "$Date: 1997/12/12 06:52:17 $"
+(defconst irchat-client-version-release "$Date: 1997/12/12 06:54:14 $"
   "version release date")
 
 (defconst irchat-client-name "Irchat"
