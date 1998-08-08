a95371ad415c88df137ba9da4f0abf7a78620bec
tri <tri>
1998-08-08 14:56:08 +0000

    Made irchat-caesar-string and irchat-caesar-region is 	now just a wrapper for this one.

--- irchat-caesar.el
+++ irchat-caesar.el
@@ -1,6 +1,6 @@
 ;;;  -*- emacs-lisp -*-
 ;;;
-;;;  $Id: irchat-caesar.el,v 3.2 1997/02/27 13:29:27 tri Exp $
+;;;  $Id: irchat-caesar.el,v 3.3 1998/08/08 14:56:08 tri Exp $
 ;;;
 ;;; caesar-region written by phr@prep.ai.mit.edu  Nov 86
 ;;; Modified by tower@prep Nov 86
@@ -9,12 +9,9 @@
 
 (eval-and-compile (provide 'irchat-caesar))
 
-(defun irchat-caesar-region (&optional n &optional japanese)
-  "Caesar rotation of region by N, default 13, for decrypting netnews.
+(defun irchat-caesar-string (str &optional n copy japanese)
+  "Caesar rotation of STRING by N, default 13, for decrypting netnews.
 ROT47 will be performed for Japanese text in any case."
-  (interactive (if current-prefix-arg	; Was there a prefix arg?
-		   (list (prefix-numeric-value current-prefix-arg))
-		 (list nil nil)))
   (cond ((not (numberp n)) (setq n 13))
 	((< n 0) (setq n (- 26 (% (- n) 26))))
 	(t (setq n (% n 26))))		;canonicalize N
@@ -65,18 +62,30 @@ ROT47 will be performed for Japanese text in any case."
 	(let ((caesar-translate-table (if japanese 
 					  caesar-translate-table-2
 					caesar-translate-table-1))
-	      (from (region-beginning))
-	      (to (region-end))
-	      (i 0) str len)
-	  (setq str (buffer-substring from to))
+	      (str (if copy (substring str 0 (length str)) str))
+	      (i 0) 
+	      len)
 	  (setq len (length str))
 	  (while (< i len)
 	    (aset str i (aref caesar-translate-table (aref str i)))
 	    (setq i (1+ i)))
-	  (goto-char from)
-	  (delete-region from to)
-	  (insert str)))))
+	  str))))
 
+
+(defun irchat-caesar-region (str &optional n &optional japanese)
+  "Caesar rotation of region by N, default 13, for decrypting netnews.
+ROT47 will be performed for Japanese text in any case."
+  (interactive (if current-prefix-arg	; Was there a prefix arg?
+		   (list (prefix-numeric-value current-prefix-arg))
+		 (list nil nil)))
+  (let* ((from (region-beginning))
+	 (to (region-end))
+	 (str (buffer-substring from to)))
+    (setq str (irchat-caesar-string str n nil japanese))
+    (goto-char from)
+    (delete-region from to)
+    (insert str)))
+  
 ;;;
 ;;; eof
 ;;;
