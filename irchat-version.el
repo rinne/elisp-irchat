0af0e19c268634463a9edce9f1a37bf5383b86f7
tri <tri>
2002-06-04 15:45:33 +0000

    Automatic commit on distribution generation.

--- irchat-version.el
+++ irchat-version.el
@@ -1,10 +1,10 @@
 ;;;  -*- emacs-lisp -*-
 ;;;
-;;;  $Id: irchat-version.el,v 3.108 2002/06/04 15:12:51 tri Exp $
+;;;  $Id: irchat-version.el,v 3.109 2002/06/04 15:45:33 tri Exp $
 ;;;
 ;;; see file irchat-copyright.el for change log and copyright info
 
-;;; DO NOT TOUCH THIS LINE: ZZZXXX>>>1020356791<<<XXXZZZ
+;;; DO NOT TOUCH THIS LINE: ZZZXXX>>>1023205533<<<XXXZZZ
 
 ;;;
 ;;; Version number constants
@@ -25,7 +25,7 @@
 (defconst irchat-client-version-rcs-snap nil
   "*If a `current' release, what's the date?")
 
-(defconst irchat-client-version-release "$Date: 2002/06/04 15:12:51 $"
+(defconst irchat-client-version-release "$Date: 2002/06/04 15:45:33 $"
   "*Version release date")
 
 (defconst irchat-client-name "Irchat"
