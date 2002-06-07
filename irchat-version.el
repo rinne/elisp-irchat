99d8a13361a7acaa6b237b6dacded83156a69527
tri <tri>
2002-06-07 13:10:07 +0000

    Automatic commit on distribution generation.

--- irchat-version.el
+++ irchat-version.el
@@ -1,6 +1,6 @@
 ;;;  -*- emacs-lisp -*-
 ;;;
-;;;  $Id: irchat-version.el,v 3.138 2002/06/07 13:10:06 tri Exp $
+;;;  $Id: irchat-version.el,v 3.139 2002/06/07 13:10:08 tri Exp $
 ;;;
 ;;; see file irchat-copyright.el for change log and copyright info
 
@@ -19,19 +19,19 @@
 (defconst irchat-client-version-beta-p t
   "*Is this version a beta version?")
 
-(defconst irchat-client-version-alpha-p nil
+(defconst irchat-client-version-alpha-p t
   "*Is this version an alpha version?")
 
-(defconst irchat-client-version-rcs-snap nil
+(defconst irchat-client-version-rcs-snap t
   "*Is this just a snapshot from rcs?")
 
-(defconst irchat-client-version-release "$Date: 2002/06/07 13:10:06 $"
+(defconst irchat-client-version-release "$Date: 2002/06/07 13:10:08 $"
   "*Version release date")
 
 (defconst irchat-client-name "Irchat"
   "*Name of this program.")
 
-(defconst irchat-client-distribution "irchat-4.00-beta2"
+(defconst irchat-client-distribution "none"
   "*What is the buld name of this distribution?")
 
 (defconst irchat-emacs-version (let ((e-v-s (if (boundp 'emacs-version) 
