8f934080dd2cc69fdf0f5c1eed3e5b8ab6bffe22
tri <tri>
2009-08-19 17:49:08 +0000

    Automatic commit on distribution generation.

--- irchat-version.el
+++ irchat-version.el
@@ -1,6 +1,6 @@
 ;;;  -*- emacs-lisp -*-
 ;;;
-;;;  $Id: irchat-version.el,v 3.264 2009/08/19 17:48:53 tri Exp $
+;;;  $Id: irchat-version.el,v 3.265 2009/08/19 17:49:08 tri Exp $
 ;;;
 ;;; see file irchat-copyright.el for change log and copyright info
 
@@ -22,16 +22,16 @@
 (defconst irchat-client-version-alpha-p t
   "*Is this version an alpha version?")
 
-(defconst irchat-client-version-rcs-snap t
+(defconst irchat-client-version-rcs-snap nil
   "*Is this just a snapshot from rcs?")
 
-(defconst irchat-client-version-release "$Date: 2009/08/19 17:48:53 $"
+(defconst irchat-client-version-release "$Date: 2009/08/19 17:49:08 $"
   "*Version release date")
 
 (defconst irchat-client-name "Irchat"
   "*Name of this program.")
 
-(defconst irchat-client-distribution "none"
+(defconst irchat-client-distribution "irchat-5.02"
   "*What is the buld name of this distribution?")
 
 (defconst irchat-emacs-version-major
